0d840cb9647bec89a43d7213aa492e77de31f859
Sivlia <silvolivm@gmail.com>
2023-03-11 20:53:34 +0100

    add modification archive

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -4,6 +4,7 @@ import { User } from '../entities/user';
 import { Repo } from '../repository/repo.interface';
 import { HTTPError } from '../interfaces/error.js';
 import { Auth, PayloadToken } from '../services/auth.js';
+import { url } from 'inspector';
 const debug = createDebug('RRSS:controller:users');
 export class UsersController {
   constructor(public repo: Repo<User>) {
@@ -14,6 +15,21 @@ export class UsersController {
     try {
       debug('getAll');
       const data = await this.repo.query();
+      resp.json({
+        results: data,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getById(req: Request, resp: Response, next: NextFunction) {
+    try {
+      debug('getById');
+      const getId = req.params.id;
+      debug(getId);
+
+      const data = await this.repo.queryId(getId);
       resp.json({
         results: data,
       });
@@ -73,6 +89,21 @@ export class UsersController {
       resp.status(202);
       resp.json({
         token,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async delete(req: Request, resp: Response, next: NextFunction) {
+    try {
+      debug('delete');
+      const deleteId = req.params.id;
+      debug(deleteId);
+
+      const data = await this.repo.destroy(deleteId);
+      resp.json({
+        results: [],
       });
     } catch (error) {
       next(error);
